65c497518d9b73e04c2e04dc0ea6ce41b65cd33b
Kirill-Dr <daright6@gmail.com>
2025-06-28 21:36:05 +0600

    feat: made more functions with return

--- BMICalculator/main.go
+++ BMICalculator/main.go
@@ -6,19 +6,29 @@ import (
 )
 
 func main() {
-	const IMTPower = 2
-	var userHeight float64
-	var userWeight float64
 	fmt.Println("___Calculator of BMI___")
-	fmt.Print("Enter your height (cm): ")
-	fmt.Scan(&userHeight)
-	fmt.Print("Enter your weight (kg): ")
-	fmt.Scan(&userWeight)
-	BMI := userWeight / math.Pow(userHeight/100, IMTPower)
+	userWeight, userHeight := getUserInput()
+	BMI := calculateBMI(userWeight, userHeight)
 	outputResult(BMI)
 }
 
 func outputResult(BMI float64) {
 	result := fmt.Sprintf("Your BMI is: %.0f", BMI)
 	fmt.Println(result)
+}
+
+func calculateBMI(userWeight float64, userHeight float64) float64 {
+	const IMTPower = 2
+	BMI := userWeight / math.Pow(userHeight/100, IMTPower)
+	return BMI
+}
+
+func getUserInput() (float64, float64) {
+	var userWeight float64
+	var userHeight float64
+	fmt.Print("Enter your weight (kg): ")
+	fmt.Scan(&userWeight)
+	fmt.Print("Enter your height (cm): ")
+	fmt.Scan(&userHeight)
+	return userWeight, userHeight
 }
