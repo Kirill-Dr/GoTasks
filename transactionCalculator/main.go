540dbaf89f3006eae22934070b7629ef83824984
Kirill-Dr <daright6@gmail.com>
2025-06-29 22:27:16 +0600

    feat: added transaction writing

--- transactionCalculator/main.go
+++ transactionCalculator/main.go
@@ -1,11 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	transactions := []int{0, 20, 35}
-	temp := transactions
-	transactions = append(transactions, 100)
-	fmt.Println(temp)
+	transactions := []float64{}
+	for {
+		transaction, err := scanTransaction()
+		if transaction == 0 {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		transactions = append(transactions, transaction)
+	}
 	fmt.Println(transactions)
+}
+
+func scanTransaction() (float64, error) {
+	var transaction float64
+	fmt.Println("Enter transaction (n to finish): ")
+	_, err := fmt.Scan(&transaction)
+	if err != nil {
+		var discard string
+		fmt.Scanln(&discard)
+		return 0, errors.New("Transaction is not valid")
+	}
+	return transaction, nil
 }
