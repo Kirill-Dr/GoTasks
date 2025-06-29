b7eca99c80614769ceec460d360d844642140b88
Kirill-Dr <daright6@gmail.com>
2025-06-29 19:27:18 +0600

    feat: array

---
+++ transactionCalculator/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	transactions := [5]int{1, 2, 3, 4, 5}
+	banks := [2]string{}
+
+	fmt.Println(transactions[1])
+	banks[0] = "Тинькофф"
+	fmt.Println(banks)
+}
