5ed31b566d9fa2abbf5f7196bfaee59ea9df2035
Kirill-Dr <daright6@gmail.com>
2025-06-30 02:29:28 +0600

    feat: calculate function

--- transactionCalculator/main.go
+++ transactionCalculator/main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	transactions := []float64{}
+	fmt.Println("--- Transaction Calculator ---")
 	for {
 		transaction, err := scanTransaction()
 		if transaction == 0 {
@@ -18,12 +19,13 @@ func main() {
 		}
 		transactions = append(transactions, transaction)
 	}
-	fmt.Println(transactions)
+	balance := calculateBalance(transactions)
+	fmt.Printf("Your balance is: %.2f\n", balance)
 }
 
 func scanTransaction() (float64, error) {
 	var transaction float64
-	fmt.Println("Enter transaction (n to finish): ")
+	fmt.Println("Enter transaction (0 to finish): ")
 	_, err := fmt.Scan(&transaction)
 	if err != nil {
 		var discard string
@@ -31,4 +33,12 @@ func scanTransaction() (float64, error) {
 		return 0, errors.New("Transaction is not valid")
 	}
 	return transaction, nil
+}
+
+func calculateBalance(transactions []float64) float64 {
+	balance := 0.0
+	for _, value := range transactions {
+		balance += value
+	}
+	return balance
 }
