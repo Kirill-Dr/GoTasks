d85ce80a79baa7ca958586990301fa1dc0202abd
Kirill-Dr <daright6@gmail.com>
2025-07-08 16:55:30 +0600

    feat: updating map

--- bookmarks/main.go
+++ bookmarks/main.go
@@ -7,4 +7,12 @@ func main() {
 		"First": "https://first.com",
 	}
 	fmt.Println(m)
+	fmt.Println(m["First"])
+	m["First"] = "https://firstUpdated.com"
+	fmt.Println(m)
+	m["Second"] = "https://second.com"
+	m["Third"] = "https://third.com"
+	fmt.Println(m)
+	delete(m, "Third")
+	fmt.Println(m)
 }
