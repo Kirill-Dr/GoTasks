3ccbea3a7a75e14fff2fe9ad0226dad2799b3406
Kirill-Dr <daright6@gmail.com>
2025-07-11 22:24:59 +0600

    feat: added generatePassword func

--- passwordManager/main.go
+++ passwordManager/main.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+)
 
 type account struct {
 	login    string
 	password string
 	url      string
 }
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
 func main() {
 	login := promptData("Enter login: ")
@@ -31,4 +36,12 @@ func promptData(prompt string) string {
 
 func outputPassword(acc *account) {
 	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func generatePassword(n int) string {
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = letters[rand.IntN(len(letters))]
+	}
+	return string(res)
 }
