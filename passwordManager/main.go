eaa397f4a5a651aed1a5541a114612c56798479d
Kirill-Dr <daright6@gmail.com>
2025-07-13 17:06:37 +0600

    feat: menu

--- passwordManager/main.go
+++ passwordManager/main.go
@@ -7,7 +7,32 @@ import (
 )
 
 func main() {
-	createAccount()
+	fmt.Println("--- Password Manager ---")
+Menu:
+	for {
+		choice := getMenu()
+		switch choice {
+		case 1:
+			createAccount()
+		case 2:
+			findAccount()
+		case 3:
+			deleteAccount()
+		default:
+			break Menu
+		}
+	}
+}
+
+func getMenu() int {
+	var choice int
+	fmt.Println("Choose an option:")
+	fmt.Println("1. Create account")
+	fmt.Println("2. Find account")
+	fmt.Println("3. Delete account")
+	fmt.Println("4. Exit")
+	fmt.Scan(&choice)
+	return choice
 }
 
 func createAccount() {
@@ -27,6 +52,10 @@ func createAccount() {
 	}
 	files.WriteFile(file, "data.json")
 }
+
+func findAccount() {}
+
+func deleteAccount() {}
 
 func promptData(prompt string) string {
 	fmt.Print(prompt)
