bec65aa65870d5313763101a3a93fb9e7729d436
Kirill-Dr <daright6@gmail.com>
2025-07-14 16:15:18 +0600

    feat: reading and writing bins in modules

--- 3-struct/file/file.go
+++ 3-struct/file/file.go
@@ -1,5 +1,44 @@
 package file
 
-func ReadFile() {}
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
-func WriteFile() {}
+type JsonFile struct {
+	filename string
+}
+
+func NewJsonFile(filename string) *JsonFile {
+	return &JsonFile{
+		filename: filename,
+	}
+}
+
+func (file *JsonFile) ReadFile() ([]byte, error) {
+	data, err := os.ReadFile(file.filename)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func (file *JsonFile) IsJsonFile() bool {
+	return strings.HasSuffix(file.filename, ".json")
+}
+
+func (file *JsonFile) WriteFile(content []byte) {
+	data, err := os.Create(file.filename)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer data.Close()
+	_, err = data.Write(content)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+	fmt.Println("File written successfully")
+}
--- 3-struct/storage/storage.go
+++ 3-struct/storage/storage.go
@@ -1,1 +1,52 @@
 package storage
+
+import (
+	"encoding/json"
+	"errors"
+
+	"3-struct/bins"
+	"3-struct/file"
+)
+
+type Storage struct {
+	jsonFile *file.JsonFile
+}
+
+func NewStorage(filename string) *Storage {
+	return &Storage{
+		jsonFile: file.NewJsonFile(filename),
+	}
+}
+
+func (storage *Storage) SaveBins(binList *bins.BinList) error {
+	if !storage.jsonFile.IsJsonFile() {
+		return errors.New("file is not a JSON file")
+	}
+
+	data, err := json.Marshal(binList)
+	if err != nil {
+		return errors.New("error marshalling data")
+	}
+
+	storage.jsonFile.WriteFile(data)
+	return nil
+}
+
+func (storage *Storage) ReadBins() (*bins.BinList, error) {
+	if !storage.jsonFile.IsJsonFile() {
+		return nil, errors.New("file is not a JSON file")
+	}
+
+	data, err := storage.jsonFile.ReadFile()
+	if err != nil {
+		return nil, errors.New("error reading file")
+	}
+
+	var binList bins.BinList
+	err = json.Unmarshal(data, &binList)
+	if err != nil {
+		return nil, errors.New("error decoding JSON")
+	}
+
+	return &binList, nil
+}
