33d931e84aea8cc6d3561ee1bec35ac0af7e5730
Kirill-Dr <daright6@gmail.com>
2025-07-14 18:17:25 +0600

    feat: interface

--- passwordManager/account/vault.go
+++ passwordManager/account/vault.go
@@ -2,12 +2,16 @@ package account
 
 import (
 	"encoding/json"
-	"passwordManager/files"
 	"strings"
 	"time"
 
 	"github.com/fatih/color"
 )
+
+type DB interface {
+	Read() ([]byte, error)
+	Write([]byte)
+}
 
 type Vault struct {
 	Accounts  []Account `json:"accounts"`
@@ -16,10 +20,10 @@ type Vault struct {
 
 type VaultWithDB struct {
 	Vault
-	db files.JsonDB
+	db DB
 }
 
-func NewVault(db *files.JsonDB) *VaultWithDB {
+func NewVault(db DB) *VaultWithDB {
 	file, err := db.Read()
 	if err != nil {
 		return &VaultWithDB{
@@ -27,7 +31,7 @@ func NewVault(db *files.JsonDB) *VaultWithDB {
 				Accounts:  []Account{},
 				UpdatedAt: time.Now(),
 			},
-			db: *db,
+			db: db,
 		}
 	}
 	var vault Vault
@@ -39,12 +43,12 @@ func NewVault(db *files.JsonDB) *VaultWithDB {
 				Accounts:  []Account{},
 				UpdatedAt: time.Now(),
 			},
-			db: *db,
+			db: db,
 		}
 	}
 	return &VaultWithDB{
 		Vault: vault,
-		db:    *db,
+		db:    db,
 	}
 }
 
