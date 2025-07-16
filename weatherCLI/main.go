29f5c304f5c804b2da6ee235210a1aaca8ab3ce8
Kirill-Dr <daright6@gmail.com>
2025-07-16 13:17:41 +0600

    feat: new reader

--- weatherCLI/main.go
+++ weatherCLI/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"strings"
 )
 
 func main() {
@@ -10,4 +12,13 @@ func main() {
 	format := flag.Int("format", 1, "Format of output")
 	flag.Parse()
 	fmt.Println(*city, *format)
+
+	reader := strings.NewReader("Hello")
+	block := make([]byte, 4)
+	for {
+		if _, err := reader.Read(block); err == io.EOF {
+			break
+		}
+		fmt.Printf("%q\n", block)
+	}
 }
