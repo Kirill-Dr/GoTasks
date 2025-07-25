b083f95d48216ff8894913d4a046443e5fded593
Kirill-Dr <daright6@gmail.com>
2025-07-25 14:42:37 +0600

    ref: removed waitgroup

--- gorutinePractice/main.go
+++ gorutinePractice/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
 func sumNums(nums []int, ch chan int) {
@@ -14,7 +13,6 @@ func sumNums(nums []int, ch chan int) {
 }
 
 func main() {
-	var wg sync.WaitGroup
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
 	numGoroutines := 3
 	ch := make(chan int, numGoroutines)
@@ -22,26 +20,16 @@ func main() {
 	partSize := len(arr) / numGoroutines
 
 	for i := range numGoroutines {
-		wg.Add(1)
-
 		start := i * partSize
 		end := start + partSize
 
-		go func() {
-			defer wg.Done()
-			sumNums(arr[start:end], ch)
-		}()
+		go sumNums(arr[start:end], ch)
 	}
 
 	totalSum := 0
 	for range numGoroutines {
 		totalSum += <-ch
 	}
-
-	go func() {
-		wg.Wait()
-		close(ch)
-	}()
 
 	fmt.Println("Total Sum:", totalSum)
 }
